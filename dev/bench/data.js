1418a43b8820405ca4f7ae251ee8316c67419b9b
github-action-benchmark <github@users.noreply.github.com>
2024-08-22 14:13:07 +0000

    add Test Benchmark (cargo) benchmark result for 0bac6b043ec1c01260c22ba8c05ed91b7a6790bd

---
+++ dev/bench/data.js
@@ -0,0 +1,35 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1724335987098,
+  "repoUrl": "https://github.com/cryspen/test-merge-queue-actions",
+  "entries": {
+    "Test Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "Jan Winkelmann (keks)",
+            "email": "jan@cryspen.com"
+          },
+          "committer": {
+            "name": "Jan Winkelmann",
+            "username": "keks",
+            "email": "146678+keks@users.noreply.github.com"
+          },
+          "id": "0bac6b043ec1c01260c22ba8c05ed91b7a6790bd",
+          "message": "libtest? more like libtrash am i right",
+          "timestamp": "2024-08-22T14:07:58Z",
+          "url": "https://github.com/cryspen/test-merge-queue-actions/commit/0bac6b043ec1c01260c22ba8c05ed91b7a6790bd"
+        },
+        "date": 1724335986239,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fib 20",
+            "value": 20422,
+            "range": "± 311",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
