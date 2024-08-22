8dda54197575159656920203c9b31db1fad078ae
github-action-benchmark <github@users.noreply.github.com>
2024-08-22 14:29:38 +0000

    add Test Benchmark (cargo) benchmark result for eee6f9d788e8563e5951206bf12f8f88902fba39

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1724335987098,
+  "lastUpdate": 1724336978098,
   "repoUrl": "https://github.com/cryspen/test-merge-queue-actions",
   "entries": {
     "Test Benchmark": [
@@ -26,6 +26,33 @@ window.BENCHMARK_DATA = {
             "name": "fib 20",
             "value": 20422,
             "range": "± 311",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Jan Winkelmann (keks)",
+            "email": "jan@cryspen.com"
+          },
+          "committer": {
+            "name": "Jan Winkelmann",
+            "username": "keks",
+            "email": "146678+keks@users.noreply.github.com"
+          },
+          "id": "eee6f9d788e8563e5951206bf12f8f88902fba39",
+          "message": "hmmmmmm",
+          "timestamp": "2024-08-22T14:27:51Z",
+          "url": "https://github.com/cryspen/test-merge-queue-actions/commit/eee6f9d788e8563e5951206bf12f8f88902fba39"
+        },
+        "date": 1724336977685,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fib 20",
+            "value": 20419,
+            "range": "± 87",
             "unit": "ns/iter"
           }
         ]
